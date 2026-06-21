77c5a21acd664ebfc97d30ba915b0ad48e22eeb4
Ariel Tshibangu <ariel243tshibangu@gmail.com>
2026-06-21 16:41:21 +0100

    Initial Strapi project

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,8 +7,8 @@ export default [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'img-src': ["'self'", 'data:', 'blob:'],
-          'media-src': ["'self'", 'data:', 'blob:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
         },
       },
     },
@@ -21,9 +21,12 @@ export default [
         'http://127.0.0.1:3000',
         'http://172.24.128.1:3000',
         'http://172.20.10.4:3000',
-        'http://localhost:1337',
+
+        // ✅ TON FRONTEND EN PRODUCTION (TRÈS IMPORTANT)
+        'https://media-a8di.onrender.com',
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: '*',
     },
   },
   'strapi::poweredBy',
